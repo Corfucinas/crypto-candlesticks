d7d234b6dc7e1894845a9445b0405be05be91941
Corfucinas <corfucinas@protonmail.com>
2020-07-07 00:38:01 +0800

    add tests

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""Package-wide test fixtures."""
+from unittest.mock import Mock
+
+import pytest
+from _pytest.config import Config
+from pytest_mock import MockFixture
+
+
+def pytest_configure(config: Config) -> None:
+    """Pytest configuration hook."""
+    config.addinivalue_line('markers', 'e2e: mark as end-to-end test.')
+
+
+@pytest.fixture  # type: ignore
+def mock_requests_get(mocker: MockFixture) -> Mock:
+    """Fixture for mocking requests.get."""
+    mock = mocker.patch('requests.get')
+    mock.return_value.__enter__.return_value.json.return_value = {
+        'btc': 'usd',
+        '400': [],
+    }
+    return mock
---
+++ tests/test_interface.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+from unittest.mock import MagicMock, Mock
+
+import pytest
+import requests
+from click.testing import CliRunner
+from pytest_mock import MockFixture, mocker
+
+from crypto_candlesticks import interface
+from crypto_candlesticks.bitfinex_connector.connector import Connector
+from crypto_candlesticks.database import SqlDatabase
+
+
+@pytest.fixture  # type: ignore
+def runner() -> CliRunner:
+    """Fixture for invoking command-line interfaces."""
+    return CliRunner()
+
+
+def test_main_succeeds(runner: CliRunner, mock_requests_get: Mock) -> None:
+    """Exits with a status code of zero."""
+    result = runner.invoke(
+        interface.main,
+        args=[
+            '-s',
+            'btc',
+            '-b',
+            'usd',
+            '-i',
+            '1m',
+            '-sd',
+            '2020-01-01',
+            '-ed',
+            '2020-01-01',
+        ],
+    )
+    assert result.exit_code == 0
+
+
+@pytest.mark.e2e  # type: ignore
+def test_main_succeeds_in_production_env(runner: CliRunner) -> None:
+    """Exits with a status code of zero."""
+    result = runner.invoke(
+        interface.main,
+        args=[
+            '-s',
+            'btc',
+            '-b',
+            'usd',
+            '-i',
+            '1m',
+            '-sd',
+            '2020-01-01',
+            '-ed',
+            '2020-01-01',
+        ],
+    )
+    assert result.exit_code == 0
+
+
+def test_main_fails(runner: CliRunner) -> None:
+    """Exits with a status code of two."""
+    result = runner.invoke(
+        interface.main,
+        args=[
+            '-s',
+            'CTT',
+            '-b',
+            'TTC',
+            '-i',
+            '20m',
+            '-sd',
+            '1990-01-01',
+            '-ed',
+            '3000-01-01',
+        ],
+    )
+    assert result.exit_code == 2
+
+
+@pytest.mark.e2e  # type: ignore
+def test_main_fails_in_production_env(runner: CliRunner) -> None:
+    """Exits with a status code of two (end-to-end)."""
+    result = runner.invoke(
+        interface.main,
+        args=[
+            '-s',
+            'CTT',
+            '-b',
+            'TTC',
+            '-i',
+            '20m',
+            '-sd',
+            '1990-01-01',
+            '-ed',
+            '3000-01-01',
+        ],
+    )
+    assert result.exit_code == 2
+
+
+def test_main_fails_on_request_error(
+    runner: CliRunner, mock_requests_get: Mock
+) -> None:
+    """It exits with a non-zero status code if the request fails."""
+    mock_requests_get.side_effect = Exception('Boom')
+    result = runner.invoke(
+        interface.main,
+        args=[
+            '-s',
+            'btc',
+            '-b',
+            'usd',
+            '-i',
+            '1m',
+            '-sd',
+            '2020-01-01',
+            '-ed',
+            '2020-01-01',
+        ],
+    )
+    assert result.exit_code == 1
+
+
+def test__repr__():
+    db = SqlDatabase('test.db')
+    assert db.__repr__() == 'Sql database class'
