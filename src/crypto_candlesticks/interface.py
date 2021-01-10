378530b4f5371f9f990f7b20b31fd0527c17d45d
Corfucinas <53630064+Corfucinas@users.noreply.github.com>
2021-01-10 19:30:18 +0800

    move welcome prompt

--- src/crypto_candlesticks/interface.py
+++ src/crypto_candlesticks/interface.py
@@ -10,6 +10,8 @@ from crypto_candlesticks.get_data import get_data
 from crypto_candlesticks.symbols.intervals import intervals
 from crypto_candlesticks.symbols.quote_currency import quote_currency
 
+click.secho('Welcome, what data do you wish to download?', fg='green')
+
 
 @click.command()
 @click.option(
@@ -103,5 +105,4 @@ def main(
 
 
 if __name__ == '__main__':
-    click.secho('Welcome, what data do you wish to download?', fg='green')
     main()
