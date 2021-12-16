5010b1db07f1eb0e06aaa3076d99f0d056ff0784
Corfucinas <53630064+Corfucinas@users.noreply.github.com>
2021-12-16 18:44:22 +0800

    increase RATE_LIMIT to avoid timeouts once again

--- src/crypto_candlesticks/get_data.py
+++ src/crypto_candlesticks/get_data.py
@@ -14,7 +14,7 @@ from crypto_candlesticks.database import SqlDatabase
 from crypto_candlesticks.exchanges.bitfinex import Bitfinex
 from crypto_candlesticks.text_console import setup_table, write_to_console
 
-_RATE_LIMIT = 1.0201
+_RATE_LIMIT = 5.0201
 _STEP_SIZE = 86400000
 
 
