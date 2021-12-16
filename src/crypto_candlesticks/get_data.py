775a58135579ec5aaf55243dafa25409138d01a6
Corfucinas <53630064+Corfucinas@users.noreply.github.com>
2021-12-17 01:14:35 +0800

    decrease RATE_LIMIT

--- src/crypto_candlesticks/get_data.py
+++ src/crypto_candlesticks/get_data.py
@@ -14,7 +14,7 @@ from crypto_candlesticks.database import SqlDatabase
 from crypto_candlesticks.exchanges.bitfinex import Bitfinex
 from crypto_candlesticks.text_console import setup_table, write_to_console
 
-_RATE_LIMIT = 1.85
+_RATE_LIMIT = 1.82
 _STEP_SIZE = 86400000
 
 
