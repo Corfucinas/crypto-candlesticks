85a20ad179a658d8dcc0ce2802c805e3053bb6fc
Corfucinas <53630064+Corfucinas@users.noreply.github.com>
2022-01-25 23:40:58 +0800

    refactor(conf): remove encoding pragma

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
 """Sphinx configuration."""
 from datetime import datetime
 
 project = 'Crypto Candlesticks'
 author = 'Pedro Torres'
-copyright = f'{datetime.now().year}, {author}'
+year = datetime.now().year
+copyright = f'{year}, {author}'
 extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.autodoc',
